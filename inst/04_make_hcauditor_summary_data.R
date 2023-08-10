624776d192db09e97f59b474ed6aea3d3a967c93
Cole Brokamp <cole.brokamp@gmail.com>
2023-08-10 18:12:10 -0400

    track hamilton_online_parcels tdr

--- inst/04_make_hcauditor_summary_data.R
+++ inst/04_make_hcauditor_summary_data.R
@@ -1,5 +1,6 @@
 library(dplyr, warn.conflicts = FALSE)
 library(codec)
+Sys.unsetenv("RETICULATE_PYTHON") # where????????
 # make sure {parcel} is loaded using devtools::load_all() to access read/write paths inside package during development
 
 d <-
