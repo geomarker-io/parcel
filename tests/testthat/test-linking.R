0ec91eddda58e3a5f55a51838bf75243972a1283
Cole Brokamp <cole.brokamp@gmail.com>
2023-09-08 14:53:10 -0400

    finish testing; non-inst parcel ids must be unique

--- tests/testthat/test-linking.R
+++ tests/testthat/test-linking.R
@@ -20,26 +20,28 @@ test_that("link_parcel works", {
 
 test_that("link_parcel works with inst addresses", {
   skip_if_no_dedupe()
+
   my_addresses <- c(
-    "222 Central Pkwy Cincinnati Ohio 45220",
     "222 E Central Parkway Cincinnati Ohio 45220",
-    "3010 Eden Ave Cincinnati Ohio 45219",
+    "222 Central Pkwy Cincinnati Ohio 45220",
     "3031 Eden Ave Cincinnati Ohio 45219",
+    "3010 Eden Ave Cincinnati Ohio 45219",
     "3333 Burnet Ave Cincinnati Ohio 45219",
     "3333 Burnet Avenue Cincinnati Ohio 45219",
     "341 Erkenbrecher Ave Cincinnati Ohio 45219",
     "350 Erkenbrecher Ave Cincinnati Ohio 45219"
   )
   my_addr_links <- link_parcel(my_addresses, threshold = 0.2)
-
   out <-
     tibble::tibble(address = my_addresses) |>
     dplyr::left_join(my_addr_links, by = c("address" = "input_address"))
+  out
 
+  expect_equal(length(my_addresses), nrow(out))
 
+  expect_equal(out$parcel_id,
+               paste0("nonres-", c("jfs-e", "jfs", "eden-3031", "eden-3010", "cchmc", "cchmc", "rmh-341", "rmh-350")))
   
-  expect_equal(length(my_addresses), nrow(my_addr_links) + 1)
-  # TODO finish testing; should we remove burnet ave from parcel dataset?
 })
 
 test_that("link_parcel threshold works", {
