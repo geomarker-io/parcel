843ae254630dff858c99c1f33091338c5c3b0af9
Cole Brokamp <cole-brokamp@users.noreply.github.com>
2023-11-06 17:27:01 -0500

    link apartment complexes based on street ranges (#38)
    also deprecate the website

--- tests/testthat/test-linking.R
+++ tests/testthat/test-linking.R
@@ -14,8 +14,35 @@ test_that("link_parcel works", {
   )
   my_addr_links <- link_parcel(my_addresses, threshold = 0.2)
   expect_equal(nrow(my_addr_links), 3)
-  expect_equal(my_addr_links$parcel_id,
-               c("2170054005900", "2270001008600", "2270001008600"))
+  expect_equal(
+    my_addr_links$parcel_id,
+    c("2170054005900", "2270001008600", "2270001008600")
+  )
+})
+
+test_that("link_apt works", {
+  skip_if_no_dedupe()
+
+  link_apt("3830 President Drive Cincinnati Ohio 45225") |>
+    expect_identical("president")
+
+  # under range
+  link_apt("2998 President Drive Cincinnati Ohio 45225") |>
+    expect_identical(NA)
+
+  # over range
+  link_apt("5001 President Drive Cincinnati Ohio 45225") |>
+    expect_identical(NA)
+
+  link_apt("2582 East Tower Drive Cincinnati Ohio 45238") |>
+    expect_identical("tower")
+
+  # no matched street
+  link_apt("224 Woolper Ave Cincinnati Ohio 45225") |>
+    expect_identical(NA)
+
+  link_apt("224 Woolper Ave Cincinnati Ohio 89344") |>
+    expect_identical(NA)
 })
 
 test_that("link_parcel works with inst addresses", {
@@ -36,24 +63,29 @@ test_that("link_parcel works with inst addresses", {
     dplyr::left_join(my_addr_links, by = c("address" = "input_address"))
   out
   expect_equal(length(my_addresses), nrow(out))
-  expect_equal(out$parcel_id,
-               paste0("nonres-", c("jfs-e", "jfs", "eden-3031", "eden-3010", "cchmc", "cchmc", "rmh-341", "rmh-350")))
+  expect_equal(
+    out$parcel_id,
+    paste0("nonres-", c("jfs-e", "jfs", "eden-3031", "eden-3010", "cchmc", "cchmc", "rmh-341", "rmh-350"))
+  )
 })
 
 test_that("get_parcel_data works", {
   skip_if_no_dedupe()
   out <-
-    c("352 Helen St Cincinnati OH 45202",
-    "5377 Bahama Ter Cincinnati Ohio 45223",
-    "1851 Campbell Dr Hamilton Ohio 45011") |>
+    c(
+      "352 Helen St Cincinnati OH 45202",
+      "224 Woolper Ave Cincinnati OH 45229",
+      "5377 Bahama Ter Cincinnati Ohio 45223", # TODO will now be matched to "bahama" instead of an exact id
+      "1851 Campbell Dr Hamilton Ohio 45011"
+    ) |>
     get_parcel_data()
-  expect_equal(nrow(out), 3)
-  expect_equal(as.vector(out$homestead), c(NA, FALSE, NA))
-  expect_equal(as.vector(out$parcel_id), c(NA, "2270001008600", NA))
-  expect_equal(as.vector(out$year_built), c(NA, 1968, NA))
-  expect_equal(as.vector(out$n_total_rooms), c(NA, 8, NA))
-  expect_equal(as.vector(out$online_market_total_value), c(NA, 164610, NA))
-  })
+  expect_equal(nrow(out), 4)
+  expect_equal(as.vector(out$homestead), c(NA, FALSE, NA, NA))
+  expect_equal(as.vector(out$parcel_id), c(NA, "2170054005900", "bahama", NA))
+  expect_equal(as.vector(out$year_built), c(NA, 1912, NA, NA))
+  expect_equal(as.vector(out$n_total_rooms), c(NA, 6, NA, NA))
+  expect_equal(as.vector(out$online_market_total_value), c(NA, 265990, NA, NA))
+})
 
 test_that("get_parcel_data only returns 1 row, possibly TIED_MATCH, per input address", {
   skip_if_no_dedupe()
