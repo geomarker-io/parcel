f24690932e590dfc396d3da0762aaa0a52ec952e
Cole Brokamp <cole.brokamp@gmail.com>
2024-07-30 09:46:55 -0400

    separate folders for each data package

---
+++ auditor_online_parcels/README.md
@@ -0,0 +1,10 @@
+
+## Hamilton County Auditor Online Data
+
+The `hamilton_online_parcels` tabular data resource contains parcel characteristics scraped from [Hamilton County Auditor Online](https://wedge1.hcauditor.org/) and linked to the parcel identifers in the `cagis_parcels` tabular data resource.
+
+Create...
+
+stored...
+
+read....
--- auditor_online_parcels/source.R
+++ auditor_online_parcels/source.R
@@ -5,17 +5,20 @@
 #' @examples
 #' scrape_hamilton_parcel("2170054005900")
 #' scrape_hamilton_parcel("5000341051800")
-scrape_hamilton_parcel <- function(parcel_id){
+scrape_hamilton_parcel <- function(parcel_id) {
   haul <-
     glue::glue("https://wedge3.hcauditor.org/view/re/{parcel_id}/2022/summary") |>
     xml2::read_html() |>
     rvest::html_nodes("#property_overview_wrapper td") |>
     rvest::html_text()
-  setNames(haul[seq(from = 2, to = length(haul), by = 2)],
-           haul[seq(from = 1, to = length(haul), by = 2)])
+  setNames(
+    haul[seq(from = 2, to = length(haul), by = 2)],
+    haul[seq(from = 1, to = length(haul), by = 2)]
+  )
 }
 
 ## # to run the very long process of scraping without using a prestored result, run:
+## d <- readRDS("../cagis_parcels/cagis_parcels/cagis_parcels.rds")
 ## httr::set_config(httr::user_agent("Mozilla/5.0 (Windows NT 5.1; rv:52.0) Gecko/20100101 Firefox/52.0"))
 ## d$auditor_online <-
 ##   mappp::mappp(d$parcel_id, scrape_hamilton_parcel, parallel = FALSE, cache = TRUE, cache_name = "auditor_online_cache")
@@ -26,16 +29,21 @@ d_scrape <- readRDS("./auditor_online_scrape_2024-01-16.rds")
 
 d_auditor_online_parcels <-
   d_scrape |>
-  select(parcel_id, auditor_online) |>
-  mutate(year_built = as.integer(purrr::map_chr(auditor_online, "Year Built"))) |>
-  mutate(n_total_rooms = as.integer(purrr::map_chr(auditor_online, "Total Rooms"))) |>
-  mutate(n_bedrooms = as.integer(purrr::map_chr(auditor_online, "# Bedrooms"))) |>
-  mutate(n_full_bathrooms = as.integer(purrr::map_chr(auditor_online, "# Full Bathrooms"))) |>
-  mutate(n_half_bathrooms = as.integer(purrr::map_chr(auditor_online, "# Half Bathrooms"))) |>
-  mutate(online_market_total_value = readr::parse_number(purrr::map_chr(auditor_online, "Market Total Value"))) |>
-  select(-auditor_online)
+  dplyr::select(parcel_id, auditor_online) |>
+  dplyr::mutate(
+    year_built = as.integer(purrr::map_chr(auditor_online, "Year Built")),
+    n_total_rooms = as.integer(purrr::map_chr(auditor_online, "Total Rooms")),
+    n_bedrooms = as.integer(purrr::map_chr(auditor_online, "# Bedrooms")),
+    n_full_bathrooms = as.integer(purrr::map_chr(auditor_online, "# Full Bathrooms")),
+    n_half_bathrooms = as.integer(purrr::map_chr(auditor_online, "# Half Bathrooms")),
+    online_market_total_value = readr::parse_number(purrr::map_chr(auditor_online, "Market Total Value"))
+  ) |>
+  dplyr::select(-auditor_online)
 
-codec::dpkg_write(d_auditor_online_parcels, "auditor_online_parcels",
+codec::dpkg_write(
+  d_auditor_online_parcels,
+  name = "auditor_online_parcels",
   version = "1.1.0",
-  homepage = "https://github.com/geomarker-io/parcel"
+  homepage = "https://github.com/geomarker-io/parcel",
+  dir = "auditor_online_parcels"
 )
